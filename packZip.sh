3c8762b62ff699852d4cc8cbc18e6a587b40eca4
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2020-02-27 17:07:43 -0500

    add zip script

---
+++ packZip.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# packs Unity3DTilesWeb.zip not including StreamingAssets, which just has some bulky demo data
+# this script may not work in Git bash because zip is not available there by default
+# (use cygwin instead)
+
+dir=Unity3DTilesWeb 
+if [ ! -d $dir ]; then
+    echo "first build GenericWeb WebGL scene, see README.md#building"
+    exit 1
+fi
+
+mkdir tmp
+cd tmp
+mkdir $dir
+cp -r ../$dir/Build $dir
+cp ../$dir/index.html $dir
+zip -rp $dir.zip $dir
+mv $dir.zip ..
+cd ..
+rm -rf tmp
